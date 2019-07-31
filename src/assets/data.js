719c810c5b6b97cb0b22151cebefeb42e6e75345
Kidjapa <brunoluizk@gmail.com>
2019-07-31 15:00:07 -0300

    Finalizando curriculo

---
+++ src/assets/data.js
@@ -0,0 +1,100 @@
+let json_data = {
+    "work": [
+        {
+            "name": "Mout's - Soluções em TI",
+            "position": "Full Stack Developer",
+            "start_date": "2018-09",
+            "end_date": "present",
+            "work_website": "http://mouts.info/",
+            "summary": "Working with Web Systems team, delivering Node.js and/or PHP applications."
+        },
+        {
+            "name": "Portomontt - B2B",
+            "position": "PHP Full Developer",
+            "start_date": "2018-04",
+            "end_date": "2018-09",
+            "work_website": "http://portomontt.com.br",
+            "summary": "Working with Web Systems team, delivering PHP applications with <a href='https://symfony.com'>Symfony</a> framework. Focused in B2B and B2C e-commerces and institucional websites."
+        },
+        {
+            "name": "HBTec - CRM",
+            "position": "PHP / C++ Developer",
+            "start_date": "2009-06",
+            "end_date": "2011-02",
+            "work_website": "http://www.hbtec.com.br/",
+            "summary": "Working with Web Systems and desktop softwares, delivering C++ and PHP applications. Focused in CRM systems."
+        },
+        {
+            "name": "Freelancer",
+            "position": "Web/Desktop Developer",
+            "start_date": "2011-02",
+            "end_date": "present",
+            "work_website": "http://webnofear.com.br/",
+            "summary": "Working with Web Systems and desktop softwares, delivering C++, Node.js and PHP applications. Institucional websites, e-commerce maintenance, ERP maintenance, automation with microcontroller or Arduino board and others."
+        }
+    ],
+    "languages":[
+        {
+            "name": "Javascript",
+            "rating": "100"
+        },
+        {
+            "name": "Node.js",
+            "rating": "95"
+        },
+        {
+            "name": "PHP",
+            "rating": "100"
+        },
+        {
+            "name": "C++",
+            "rating": "80"
+        },
+        {
+            "name": "C#",
+            "rating": "70"
+        },
+        {
+            "name": "Lua",
+            "rating": "70"
+        },
+        {
+            "name": "GDScript",
+            "rating": "80"
+        },
+        {
+            "name": "Python",
+            "rating": "75"
+        }
+    ],
+    "frameworks": [
+        {
+            "name": "Laravel",
+            "rating": "90"
+        },
+        {
+            "name": "Symfony",
+            "rating": "85"
+        },
+        {
+            "name": "Angular",
+            "rating": "90"
+        },
+        {
+            "name": "Vue.js",
+            "rating": "90"
+        },
+        {
+            "name": "React",
+            "rating": "75"
+        },
+        {
+            "name": "Gulp",
+            "rating": "75"
+        },
+        {
+            "name": "Grunt",
+            "rating": "75"
+        }
+    ]
+}
